f8f6740133564f115bae97ebb57ba933158e89d2
holiday <waiyou148@126.com>
2020-03-19 23:45:21 +0800

    xxx

---
+++ src/main/scala/com/holiday/spark/App.scala
@@ -0,0 +1,15 @@
+
+
+package com.holiday.spark
+
+/**
+  * Hello world!
+  *
+  */
+object App {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+    val arr = Array(1,2,3,4)
+    arr.map()
+  }
+  }
